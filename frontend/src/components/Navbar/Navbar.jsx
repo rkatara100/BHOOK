e238a3957efe228478c71df907e978476c1655b9
rkatara100 <rkatara100@gmail.com>
2024-10-08 23:52:43 +0530

    added navbar

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
 import './Navbar.css'
+import { assets } from '../../assets/assets'
 const Navbar = () => {
+
+
+
       return (
             <div className='navbar'>
-                  Navbar
+                  <img src={assets.logo} className='logo' alt='logo' />
+                  <ul className='navbar-menu'>
+                        <li>home</li>
+                        <li>menu</li>
+                        <li>mobile-app</li>
+                        <li>contact us</li>
+                  </ul>
+
+                  <div className='navbar-right'>
+                        <img src={assets.search_icon} alt="" />
+                        <div className="navbar-search-icon">
+                              <img src={assets.basket_icon} alt="basket_icon" className="basket_icon" />
+                              <div className='dot'></div>
+                        </div>
+                        <button>sign in</button>
+                  </div>
+
+
+
             </div>
       )
 }
