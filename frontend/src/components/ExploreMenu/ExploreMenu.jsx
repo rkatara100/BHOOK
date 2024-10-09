f2654d510ef4676e6f39606de9fb6eab2b911d68
rkatara100 <rkatara100@gmail.com>
2024-10-09 19:05:18 +0530

    added header

---
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../ExploreMenu/ExploreMenu.css';
+
+const ExploreMenu = () => {
+      return (
+            <div className='ExploreMenu'>
+
+            </div>
+      )
+}
+
+export default ExploreMenu
---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../Header/Header.css'
+
+const Header = () => {
+      return (
+            <div className='header'>
+                  <div className="header-contents">
+                        <h2>Order your favourite food here</h2>
+                        <p> Choose from a diverse menu get the best dishesh quickly and in time from your nearest restaurant. </p>
+                        <button>View</button>
+                  </div>
+            </div>
+      )
+}
+
+export default Header
