863a3f0be99f73f2f933c936617fa2cc21e0d43a
rkatara100 <rkatara100@gmail.com>
2024-10-17 00:00:05 +0530

    added cart

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -18,7 +18,7 @@ const Navbar = ({ setShowLogin }) => {
 
       return (
             <div className='navbar'>
-                  <img src={assets.logo} className='logo' alt='logo' />
+                  <Link to='/'><img src={assets.logo} className='logo' alt='logo' /></Link>
                   <ul className='navbar-menu'>
                         <Link to='/' onClick={() => { setMenu("home") }} className={menu === 'home' ? 'active' : ''}>home</Link>
                         <a href='#explore-menu' onClick={() => { setMenu("menu") }} className={menu === 'menu' ? 'active' : ''}>menu</a>
@@ -29,7 +29,7 @@ const Navbar = ({ setShowLogin }) => {
                   <div className='navbar-right'>
                         <img src={assets.search_icon} alt="" />
                         <div className="navbar-search-icon">
-                              <img src={assets.basket_icon} alt="basket_icon" className="basket_icon" />
+                              <Link to='/cart'> <img src={assets.basket_icon} alt="basket_icon" className="basket_icon" /></Link>
                               <div className='dot'></div>
                         </div>
                         <button onClick={() => setShowLogin(true)}>sign in</button>
